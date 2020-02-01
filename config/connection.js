1a22d4d3aaf1ea7f50d859d6505d597652970ccd
Josh Craig <jcraig451@yahoo.com>
2020-02-01 11:25:43 -0500

    added heroku jawsDB to connections.js file

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,20 @@
 // Dependencies
 var Sequelize = require("sequelize");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+
+    // Add your own password to this section 
+    password: "Bradford321*",
+    database: "recipe"
+  });
+};
+
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 var sequelize = new Sequelize("recipe", "root", "rootroot", {
   host: "localhost",
