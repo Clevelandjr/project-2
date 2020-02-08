c7766864740970098c2fb730aaf664d1abbaa84c
Josh Craig <jcraig451@yahoo.com>
2020-02-08 10:26:38 -0500

    fixed it all

--- public/assets/js/todos.js
+++ public/assets/js/todos.js
@@ -2,31 +2,13 @@
 // $("#create-recipe").on("submit", event => {
 //   event.preventDefault();
 
-<<<<<<< HEAD
-//   $.ajax({
-//     url: "/api/post",
-//     method: "POST",
-//     data: {
-//       title: $("#title").val().trim(),
-//     }
-//   })
-// });
-  // this will need to be uncommented to sent to other page 
-  // .then(newTodo => {
-  //   location.reload("all");
+  // $.ajax({
+  //   url: "/api/post",
+  //   method: "POST",
+  //   data: {
+  //     title: $("#title").val().trim(),
+  //   }
   // });
-
-
-=======
-  $.ajax({
-    url: "/api/post",
-    method: "POST",
-    data: {
-      title: $("#title").val().trim(),
-    }
-  })
-});
->>>>>>> b80c2caf6dab81d4043b52b8d3abd65800d44e03
   // todo
   // event:click on searchButton
   // Get value of input field
@@ -46,51 +28,66 @@
       url: localAPI,
     }).then(function (html) {
 
-<<<<<<< HEAD
-// $(".complete").on("change", event => {
-//   const id = $(event.target).parent().attr('data-id');
-=======
       $('html').html(html);
-      
+    
     });
 
     event.preventDefault();
     
   });
->>>>>>> b80c2caf6dab81d4043b52b8d3abd65800d44e03
 
  
    
  
+  // function renderIngredients(Ingredient){
+  //   const queryURL = "https://api.edamam.com/api/nutrition-data?app_id=7b0446a5&app_key=5221d18a089642fa6c1b4023c16a168c&ingr=$" + Ingredient ;
+  //   $.ajax({
+  //     type: "POST",
+  //     url: queryURL,
+  //     data: data,
+  //     success: success
+  //   });
+  // }
+  // // ***********************findIngredients takes user input and passes it to renderIngredients function**************************
+  //   function findIngredients(){
+  //     var nutritionInput = ("#nutrition").val().trim();
+  //     renderIngredients(nutritionInput)
+  //   }
+  //   // *************************On-click event runs findIngredients function********************************
+  //   $("#searchBTN").on("click", function(event){
+  //     $("#nutritionResult").empty().append();
+  //     event.preventDefault();
+  //     findIngredients();
+  //   }
 
- $(".complete").on("change", event => {
-   const id = $(event.target).parent().attr('data-id');
+//  $(".complete").on("change", event => {
+//    const id = $(event.target).parent().attr('data-id');
 
-   $.ajax({
-     url: `/api/todos/${id}`,
-     method: "PUT",
-     data: {
-       isCompleted: event.target.checked
-     }
-   }).then(() => {
-     console.log(`toggled ID ${id}`);
-   }).catch(err => {
-     console.log(err);
-     location.reload();
-   });
- });
+//    $.ajax({
+//      url: `/api/todos/${id}`,
+//      method: "PUT",
+//      data: {
+//        isCompleted: event.target.checked
+//      }
+//    }).then(() => {
+//      console.log(`toggled ID ${id}`);
+//    }).catch(err => {
+//      console.log(err);
+//      location.reload();
+//    });
+//  });
 
- $(".destroy").on("click", event => {
-   const id = $(event.target).parent().attr("data-id");
+//  $(".destroy").on("click", event => {
+//    const id = $(event.target).parent().attr("data-id");
 
-   $.ajax({
-     url: `/api/todos/${id}`,
-     method: "DELETE"
-   }).then(() => {
-     console.log(`deleted ID ${id}`);
-     location.reload();
-   }).catch(err => {
-     console.log(err);
-     location.reload();
-   });
- });
+//    $.ajax({
+//      url: `/api/todos/${id}`,
+//      method: "DELETE"
+//    }).then(() => {
+//      console.log(`deleted ID ${id}`);
+//      location.reload();
+//    }).catch(err => {
+//      console.log(err);
+//      location.reload();
+//    });
+//  });
